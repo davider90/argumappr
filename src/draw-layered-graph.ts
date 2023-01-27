5972a7836a8d76edb7dac60594093515392b0868
David Bendiksen <davidbendiksen@hotmail.com>
2023-01-27 12:08:55 +0100

    Throw error on undirected graphs

--- src/draw-layered-graph.ts
+++ src/draw-layered-graph.ts
@@ -1,10 +1,15 @@
+import { Graph } from "graphlib";
 import eliminateCycles from "./eliminate-cycles";
 import layerNodes from "./layer-nodes";
 import minimiseCrossings from "./minimise-crossings";
 import straightenEdges from "./straighten-edges";
 
-function drawLayeredGraph(params: any) {
-  const one = eliminateCycles(params);
+function drawLayeredGraph(graph: Graph) {
+  if (!graph.isDirected()) {
+    throw new Error("Graph must be directed for layered drawing");
+  }
+
+  const one = eliminateCycles(graph);
   const two = layerNodes(one);
   const three = minimiseCrossings(two);
   const four = straightenEdges(three);
