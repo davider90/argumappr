7235bd957bcc3b59a33d743f76481e951a32d18a
David Bendiksen <davidbendiksen@hotmail.com>
2023-03-17 23:43:26 +0100

    Begin work on crossing minimising

--- src/minimise-crossings.ts
+++ src/minimise-crossings.ts
@@ -1,5 +1,41 @@
-function minimiseCrossings(params: any) {
-  return params;
+import { Graph } from "graphlib";
+import { NodeId, RankTable } from "./utils";
+
+export default function minimiseCrossings(graph: Graph, ranks: RankTable) {
+  const rankArray = readRankTable(graph, ranks);
+
+  for (let i = 1; i < rankArray.length; i++) {
+    const previousLayer = rankArray[i - 1];
+    const currentLayer = rankArray[i];
+
+    currentLayer.forEach((node) => {
+      const predecessors = graph.predecessors(node)!;
+      let predecessorsPositionSum = 0;
+
+      predecessors.forEach((predecessor) => {
+        predecessorsPositionSum += previousLayer.indexOf(predecessor);
+      });
+
+      const averagePosition = predecessorsPositionSum / predecessors.length;
+    });
+  }
 }
 
-export default minimiseCrossings;
+function readRankTable(graph: Graph, ranks: RankTable) {
+  const rankArray: NodeId[][] = [];
+  let rankNumber = 0;
+  let layer = ranks.getRankNodes(rankNumber);
+
+  while (!!layer) {
+    rankArray[rankNumber] = [];
+
+    layer.forEach((_, node) => {
+      rankArray[rankNumber].push(node);
+    });
+
+    rankNumber++;
+    layer = ranks.getRankNodes(rankNumber);
+  }
+
+  return rankArray;
+}
