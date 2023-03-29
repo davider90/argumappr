940369092ed791c214e378157891db128cad8295
David Bendiksen <davidbendiksen@hotmail.com>
2023-03-29 12:12:30 +0200

    Add final edge handling

--- src/draw-layered-graph.ts
+++ src/draw-layered-graph.ts
@@ -3,7 +3,7 @@ import removeCycles from "./remove-cycles";
 import layerNodes from "./layer-nodes";
 import minimiseCrossings from "./minimise-crossings";
 import straightenEdges from "./straighten-edges";
-import { buildLayoutGraph } from "./utils";
+import { buildLayoutGraph, updateInputGraph } from "./utils";
 
 /**
  * Produces a hierarchical layout of a directed graph. The algorithm is based on
@@ -22,7 +22,34 @@ function drawLayeredGraph(graph: Graph) {
   const originalEdges = removeCycles(layoutGraph);
   const ranks = layerNodes(layoutGraph);
   const graphMatrix = minimiseCrossings(layoutGraph, ranks);
-  const four = straightenEdges(layoutGraph, graphMatrix);
+  straightenEdges(layoutGraph, graphMatrix);
+
+  originalEdges.deletedLoops.forEach((edge) => {
+    layoutGraph.setEdge(edge);
+  });
+
+  originalEdges.reversedEdges.forEach((edge) => {
+    const { v, w } = edge;
+    layoutGraph.removeEdge(v, w);
+    layoutGraph.setEdge(w, v);
+  });
+
+  layoutGraph.edges().forEach((edge) => {
+    const { v, w } = edge;
+    const vData = layoutGraph.node(v);
+    const wData = layoutGraph.node(w);
+    const edgeData = layoutGraph.edge(v, w);
+    layoutGraph.setEdge(edge, {
+      ...edgeData,
+      points: [
+        { x: vData.x, y: vData.y },
+        { x: (vData.x + wData.x) / 2, y: (vData.y + wData.y) / 2 },
+        { x: wData.x, y: wData.y },
+      ],
+    });
+  });
+
+  updateInputGraph(graph, layoutGraph);
 }
 
 export default drawLayeredGraph;
