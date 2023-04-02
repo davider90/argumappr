fc60629c5a1d333a5bdc455a49334a382c157c36
David Bendiksen <davidbendiksen@hotmail.com>
2023-04-02 16:26:21 +0200

    Add more edge straightening tests

--- tests/straighten-edges.spec.ts
+++ tests/straighten-edges.spec.ts
@@ -20,12 +20,67 @@ describe("Straighten Edges", () => {
 
   it("should assign x properties to nodes", () => {
     const graph = new Graph();
-    const graphMatrix: NodeId[][] = [];
+
+    graph.setDefaultNodeLabel(() => ({}));
+    graph.setNode("a");
+    graph.setNode("b");
+    graph.setNode("c");
+    graph.setEdge("a", "b");
+    graph.setEdge("b", "c");
+
+    const graphMatrix: NodeId[][] = [["a"], ["b"], ["c"]];
 
     straightenEdges(graph, graphMatrix);
 
     assert.isTrue(
       graph.nodes().every((node) => graph.node(node).x !== undefined)
+    );
+  });
+
+  it("should assign non-conflicting x properties to nodes", () => {
+    const graph = new Graph();
+
+    graph.setDefaultNodeLabel(() => ({}));
+    graph.setNode("a");
+    graph.setNode("b");
+    graph.setNode("c");
+    graph.setNode("d");
+    graph.setNode("e");
+    graph.setNode("f");
+    graph.setNode("g");
+    graph.setNode("h");
+    graph.setEdge("a", "d");
+    graph.setEdge("a", "e");
+    graph.setEdge("b", "d");
+    graph.setEdge("b", "e");
+    graph.setEdge("c", "d");
+    graph.setEdge("c", "e");
+    graph.setEdge("d", "f");
+    graph.setEdge("d", "g");
+    graph.setEdge("e", "h");
+
+    const graphMatrix: NodeId[][] = [
+      ["a", "b", "c"],
+      ["d", "e"],
+      ["f", "g", "h"],
+    ];
+
+    straightenEdges(graph, graphMatrix);
+
+    assert.isTrue(
+      graphMatrix.every((row) =>
+        row.every((node) => {
+          const nodeX = graph.node(node).x;
+
+          return row.every((otherNode) => {
+            if (node === otherNode) return true;
+
+            const otherNodeX = graph.node(otherNode).x;
+
+            return nodeX !== otherNodeX;
+          });
+        })
+      )
     );
   });
 });
