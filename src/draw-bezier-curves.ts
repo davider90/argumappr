53f158abe5e243416d52d0df89a9227e85dd167b
David Bendiksen <davidbendiksen@hotmail.com>
2023-04-03 13:43:56 +0200

    Add first rendition of bezier curve drawing

---
+++ src/draw-bezier-curves.ts
@@ -0,0 +1,48 @@
+import { Graph } from "graphlib";
+
+function drawBezierCurves(graph: Graph) {
+  graph.edges().forEach((edge) => {
+    const { v, w } = edge;
+
+    const controlPoint0 = {
+      x: graph.node(v).x,
+      y: graph.node(v).y,
+    };
+    const controlPoint1 = {
+      x: graph.node(v).x,
+      y: graph.node(w).y,
+    };
+    const controlPoint2 = {
+      x: graph.node(w).x,
+      y: graph.node(w).y,
+    };
+
+    function line01(t: number) {
+      return {
+        x: (1 - t) * controlPoint0.x + t * controlPoint1.x,
+        y: (1 - t) * controlPoint0.y + t * controlPoint1.y,
+      };
+    }
+    function line12(t: number) {
+      return {
+        x: (1 - t) * controlPoint1.x + t * controlPoint2.x,
+        y: (1 - t) * controlPoint1.y + t * controlPoint2.y,
+      };
+    }
+    function connectingLine(t: number) {
+      const point01 = line01(t);
+      const point12 = line12(t);
+
+      return {
+        x: (1 - t) * point01.x + t * point12.x,
+        y: (1 - t) * point01.y + t * point12.y,
+      };
+    }
+
+    const points = [controlPoint0, connectingLine(0.5), controlPoint2];
+
+    graph.edge(edge).points = points;
+  });
+}
+
+export default drawBezierCurves;
